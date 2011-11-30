5272f1ebf1b1a609d3f1431c19650d3369048203
CJ Carey <perimosocordiae@gmail.com>
2011-11-30 17:29:32 -0500

    Bugfix: default arg to --graph caused graph generation every time

--- matlab_tools.py
+++ matlab_tools.py
@@ -8,7 +8,7 @@ def parse_args():
   p.add_argument('--grep', nargs='+', default=[], help='search for a (regex) pattern in all project files')
   p.add_argument('--defn', nargs='+', default=[], help='search for definitons in all project files')
   p.add_argument('--usage', nargs='+', default=[],help='search for usages in all project files')
-  p.add_argument('--graph', default='-', help='generate a project dependency graph')
+  p.add_argument('--graph', default=None, help='generate a project dependency graph')
   p.add_argument('--pyplot', action='store_true', help='use pyplot for the project dependency graph')
   args = p.parse_args()
   if not (args.grep or args.defn or args.usage or args.graph):
