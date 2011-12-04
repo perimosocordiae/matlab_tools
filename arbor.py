117193ed4d464beece3971c3ae4892bff0280d0b
CJ Carey <perimosocordiae@gmail.com>
2011-12-04 15:13:27 -0500

    Fixed js path issues, ignored *.pyc files

--- arbor.py
+++ arbor.py
@@ -4,14 +4,14 @@ import json
 import os,os.path
 
 def arbor(title,json_graph):
-  home = os.path.expanduser('~')
+  pwd = os.path.dirname(os.path.abspath(__file__))
   fh,fname = tempfile.mkstemp(suffix='.html',text=True)
   fh = os.fdopen(fh,'w')
   print >>fh, '<head><title>%s</title>' % title
   print >>fh, '<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js"></script>'
-  print >>fh, '<script src="%s/Dropbox/pyutils/arbor.js"></script>'%home
+  print >>fh, '<script src="%s"></script>'%os.path.join(pwd,'arbor.js')
   print >>fh, '<script> var graph =', json.dumps(json_graph), ';</script>'
-  print >>fh, '<script src="%s/Dropbox/pyutils/show_graph.js"></script>'%home
+  print >>fh, '<script src="%s"></script>'%os.path.join(pwd,'show_graph.js')
   print >>fh, '</head><body><canvas id=viewport width=600 height=600></body>'
   fh.close()
   subprocess.check_call('open '+fname,shell=True)
