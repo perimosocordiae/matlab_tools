aa3ca99acf51f7948d4d8a650a52d9ce4ac33f06
CJ Carey <perimosocordiae@gmail.com>
2013-02-12 13:20:51 -0500

    lint, adding gray bg to distinguish the canvas boundary, adding title as h1

--- arbor.py
+++ arbor.py
@@ -1,7 +1,8 @@
 import tempfile
 import subprocess
 import json
-import os,os.path
+import os
+
 
 def arbor(title,json_graph):
   pwd = os.path.dirname(os.path.abspath(__file__))
@@ -12,7 +13,7 @@ def arbor(title,json_graph):
   print >>fh, '<script src="%s"></script>'%os.path.join(pwd,'arbor.js')
   print >>fh, '<script> var graph =', json.dumps(json_graph), ';</script>'
   print >>fh, '<script src="%s"></script>'%os.path.join(pwd,'show_graph.js')
-  print >>fh, '</head><body><canvas id=viewport width=600 height=600></body>'
+  print >>fh, '<style>body {background-color: #EEE;}</style>'
+  print >>fh, '</head><body><h1>%s</h1><canvas id=viewport width=600 height=600></body>' % title
   fh.close()
   subprocess.check_call('open '+fname,shell=True)
-
